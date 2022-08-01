ccfe9c8eb2b226d2852bc2d9b791c6ef7e79cc49
Douglas C Lima <dclima@magnasistemas.com.br>
2022-08-01 01:23:36 -0300

    First Commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const getElement = (...queries) => document.querySelector(...queries);
+
+const buttonDetails = document.querySelectorAll(".card__pokedex");
+
+const openModal = () => container.classList.add(activeModalClass);
+const closeModal = () => container.classList.remove(activeModalClass);
+
+buttonDetails.forEach((card) => {
+  card.addEventListener("click", openModal);
+});
+
+const container = getElement(".modal");
+const modal = getElement(".modal__box");
+const buttonClose = getElement(".modal__btn-close");
+
+const activeModalClass = "modal__show";
+
+buttonClose.addEventListener("click", closeModal);
+container.addEventListener("click", (event) => {
+  if (modal.contains(event.target)) return;
+
+  closeModal();
+});
+var slide_hero = new Swiper(".slide__hero", {
+  effect: "fade",
+  pagination: {
+    el: ".slide__hero .main-area .slide__hero__explorer .swiper-pagination",
+  },
+});
